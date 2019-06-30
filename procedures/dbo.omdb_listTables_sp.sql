7c8c14d2a46df6bbcee1df1ff5a99f61f881b721
Chris Leonard <chris@databaseguy.com>
2019-06-29 21:17:11 -0500

    first test - will this auto-deploy?

---
+++ procedures/dbo.omdb_listTables_sp.sql
@@ -0,0 +1,20 @@
+USE OpManagerDB;
+GO
+
+CREATE OR REPLACE PROCEDURE dbo.omdb_listTables_sp
+    @type varchar(10) -- list/detail or summary/count
+AS
+
+    IF @type = 'list' SET @type = 'detail';
+    IF @type = 'count' SET @type = 'summary';
+
+    IF @type NOT IN ('detail', 'summary');
+
+    IF @type = 'detail'
+        SELECT ROW_NUMBER() AS row_number, t.object_id, t.name
+        FROM sys.tables AS t
+        ORDER BY t.name;
+    ELSE
+        SELECT COUNT(*) AS table_count
+        FROM sys.tables AS t;
+
