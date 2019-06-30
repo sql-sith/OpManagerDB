48409604f0216979f3416e000d196d604ceae773
Chris Leonard <chris@databaseguy.com>
2019-06-30 14:56:13 -0500

    [6/30/2019 2:56:13 PM] version bump for next teamcity test

--- procedures/dbo.omdb_listTables_sp.sql
+++ procedures/dbo.omdb_listTables_sp.sql
@@ -28,7 +28,7 @@ CREATE OR ALTER PROCEDURE dbo.omdb_listTables_sp
  * Version 0.88 - well lookie there ... the escape period is a vertical bar ... kind of gives me flashbacks
  * Version 0.89 - why is it so hard to make this fail??!?
  * Version 0.90 - my v1.00 goal looks iffy. going back to the first way i tried service messages
- * Version 0.91 - totally mystified   
+ * Version 0.91 - totally mystified  
  *
  ******************************************************************************************************************/
 
