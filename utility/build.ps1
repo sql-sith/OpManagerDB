ac144534e8e0c63db8755f6788b73ff1a282c7d5
Chris Leonard <chris@databaseguy.com>
2019-06-30 21:28:24 -0500

    [6/30/2019 9:28:23 PM] version bump for next teamcity test

--- utility/build.ps1
+++ utility/build.ps1
@@ -67,5 +67,5 @@ foreach ($file in (Get-Content $changedFilesFile)) {
     }
 }
 
-Write-Host "Build step complete."
+Write-Host "Build step complete. Processed {0} lines from changed files list and skipped {1} lines."
 exit 0
